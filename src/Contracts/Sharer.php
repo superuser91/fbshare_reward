417e64e3e5e2cafa110d7e892fb07a191a9ccf36
dungnt2 <dungnt2.ctv@gmail.com>
2022-02-16 16:45:19 +0700

    update code

--- src/Contracts/Sharer.php
+++ src/Contracts/Sharer.php
@@ -5,6 +5,8 @@ namespace Vgplay\FbShareReward\Contracts;
 interface Sharer
 {
     public function claimFbShareReward();
+    public function getLastShare();
     public function getShareCount();
     public function updateShareCount(): bool;
+    public function getMaxShareCount(): int;
 }
